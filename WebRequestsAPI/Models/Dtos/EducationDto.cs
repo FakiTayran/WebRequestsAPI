d614ec9428340bf52a09c667cde5f437218351da
faki tayran arıduru <fakitayranariduru@hotmail.com>
2021-04-08 09:29:49 +0300

    Properties names of classes changed

--- WebRequestsAPI/Models/Dtos/EducationDto.cs
+++ WebRequestsAPI/Models/Dtos/EducationDto.cs
@@ -8,6 +8,6 @@ namespace WebRequestsAPI.Models.Dtos
     public class EducationDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string EducationName { get; set; }
     }
 }
--- WebRequestsAPI/Models/Dtos/EducationTypeDto.cs
+++ WebRequestsAPI/Models/Dtos/EducationTypeDto.cs
@@ -9,6 +9,6 @@ namespace WebRequestsAPI.Models.Dtos
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string EducationTypeName { get; set; }
     }
 }
